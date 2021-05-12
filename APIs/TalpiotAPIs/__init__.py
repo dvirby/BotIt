913cf381d51acc28bbe7d5ffccaa965339714224
dvirby <62304551+dvirby@users.noreply.github.com>
2021-05-12 11:14:55 +0300

    Delete APIs/TalpiotAPIs directory

--- APIs/TalpiotAPIs/__init__.py
+++
@@ -1,7 +0,0 @@
-from APIs.TalpiotAPIs.User.who_is import *
-from APIs.TalpiotAPIs.User.user import *
-from APIs.TalpiotAPIs.Constraint.UserConstraint.user_constraint import *
-from APIs.TalpiotAPIs.Group.group import Group as Group
-from APIs.TalpiotAPIs.Group.matlam_group import MatlamGroup
-from APIs.TalpiotAPIs.Group.groups import *
-
